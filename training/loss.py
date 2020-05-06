f7b3e6bc7d6d3bd387986734637d0263eec1a58f
Yuma Kishi <obake2ai@gmail.com>
2020-05-06 12:41:02 +0900

    debug

--- training/loss.py
+++ training/loss.py
@@ -200,8 +200,9 @@ def get_faces_score(Df, images):
     # process_size = 224
     # images = images.resize((images.shape[0],process_size,process_size,images.shape[3])).astype(np.float32)
     total_score = 0
-    print (type(images))
-    prob, _, _ = Df.detect(tf.transpose(images, perm=[0, 2, 3, 1]))
+    print (tf.shape(images)[1],tf.shape(images)[2],tf.shape(images)[3])
+    input_image = tf.transpose(tf.slice(images, [0,0,0,0], [1,tf.shape(images)[1],tf.shape(images)[2],tf.shape(images)[3]]), perm=[1, 2, 0])
+    prob, _, _ = Df.detect(input_image)
     # for idx in range(images.shape[0]):
     #   prob, _, _ = Df.detect(images[idx])
     #   if len(prob)>1:
