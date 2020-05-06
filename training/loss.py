8d46806bca51fab733834de4d8f49453af2939aa
Yuma Kishi <obake2ai@gmail.com>
2020-05-06 16:09:56 +0900

    bug fix

--- training/loss.py
+++ training/loss.py
@@ -203,7 +203,7 @@ def get_faces_score(Df, images):
     input_image = tf.transpose(tf.reshape(tf.slice(images, [0,0,0,0], [1,-1,-1,-1]), images.get_shape()[1:]), perm=[1, 2, 0])
     with tf.Session() as sess2:
         input_data = tf.placeholder(tf.int32)
-        prob, _, _ = sess2.run(Df.detect{input_data}, feed_dict={input_data: input_image})
+        prob, _, _ = sess2.run(Df.detect(input_data), feed_dict={input_data: input_image})
     total_score += prob
     # for idx in range(images.shape[0]):
     #   prob, _, _ = Df.detect(images[idx])
